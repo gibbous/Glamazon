a2466dbc638617bf555ed9f35103720ec5e74205
Amanda Hoffman <amandah@alumni.rutgers.edu>
2016-07-24 20:14:23 -0400

    removed unicorn, added rainbow

--- db.js
+++ db.js
@@ -1,6 +1,7 @@
 //INITIALIZES THE NPM PACKAGES USED//
 var mysql = require('mysql');
 var inquirer = require('inquirer');
+var rainbow = require('ansi-rainbow');
 
 //INITIALIZES THE CONNECTION VARIABLE TO SYNC WITH A MYSQL DATABASE//
 var connection = mysql.createConnection({
@@ -16,10 +17,20 @@ connection.connect(function(err) {
     if (err) {
         console.error("error connecting: " + err.stack);
     }
+    welcome();
     makeTable();
 })
 
 //FUNCTION TO GRAB THE PRODUCTS TABLE FROM THE DATABASE AND PRINT RESULTS TO CONSOLE//
+var welcome = function(){
+    console.log(rainbow.r("°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸"));
+    console.log("");
+    console.log('Welcome to Glamazon!');
+    console.log("Your one stop cosplay shop for looks that pop");
+    console.log("");
+    console.log(rainbow.r("°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸"));
+    console.log("");
+}
 var makeTable = function() {
     //SELECTS ALL OF THE DATA FROM THE MYSQL PRODUCTS TABLE - SELECT COMMAND!
     connection.query('SELECT * FROM products', function(err, res) {
@@ -27,12 +38,12 @@ var makeTable = function() {
         //PRINTS THE TABLE TO THE CONSOLE WITH MINIMAL STYLING//
         var tab = "\t";
         console.log("ItemID\tProduct Name\tDepartment Name\tPrice\t# In Stock");
-        console.log("--------------------------------------------------------");
+        console.log("---------------------------------------------------------");
         //FOR LOOP GOES THROUGH THE MYSQL TABLE AND PRINTS EACH INDIVIDUAL ROW ON A NEW LINE//
         for (var i = 0; i < res.length; i++) {
             console.log(res[i].ItemID + tab + res[i].ProductName + tab + res[i].DepartmentName + tab + res[i].Price + tab + res[i].StockQuantity);
         }
-        console.log("--------------------------------------------------------");
+        console.log("---------------------------------------------------------");
         //RUNS THE CUSTOMER'S PROMPTS AFTER CREATING THE TABLE. SENDS res SO THE promptCustomer FUNCTION IS ABLE TO SEARCH THROUGH THE DATA//
         promptCustomer(res);
     });
@@ -46,7 +57,7 @@ var promptCustomer = function(res) {
             name: 'choice',
             message: 'What would you like to purchase?'
         }]).then(function(val) {
-            (console.log(val.choice));
+            //console.log(val.choice);
 
                 //SET THE VAR correct TO FALSE SO AS TO MAKE SURE THE USER INPUTS A VALID PRODUCT NAME//
                 var correct = false;
@@ -56,35 +67,32 @@ var promptCustomer = function(res) {
                   connection.query("SELECT * FROM products WHERE ?", [{
                               ProductName: val.choice
                             }], function(err, res) {
-                             
-                            });
+                                //console.log(res);
+                  
+                   if (res.length < 1){
+                    console.log("We're sorry, your product was not found. Please try again");
+                     //console.log(res);
+                  //console.log(val.choice);
+                    makeTable();
+                  }
 
-                  if (res != null){
-
+                  else {
                     var product = val.choice
-                    console.log(product);
                     correct = true;
 
-
                     inquirer.prompt([{
-                         type: 'input',
+                        type: 'input',
                         name: 'quantity',
                         message: 'How many would you like to purchase?',
                         validate: function(value) {
                             if ((isNaN(value)) == false){
                                 return true;
                             }
-                            return "ha ha, very funny - pick a number or go away";
+                            return "Please enter a number.";
                         }
-
                     }]).then(function(qty) {
-
-                       
-
-                           
-                        
                     //2. TODO: CHECK TO SEE IF THE AMOUNT REQUESTED IS LESS THAN THE AMOUNT THAT IS AVAILABLE//  
-                  connection.query("SELECT StockQuantity FROM products WHERE ?", [{
+                        connection.query("SELECT StockQuantity FROM products WHERE ?", [{
                               ProductName: product
                             }], function(err, res) {
                                 //console.log(res[0]);
@@ -95,28 +103,53 @@ var promptCustomer = function(res) {
                                 } else {
                                     var stock = res[0].StockQuantity - qty.quantity;
                                     console.log(stock);
-
-                                     //3. TODO: UPDATE THE MYSQL TO REDUCE THE StockQuanaity by the THE AMOUNT REQUESTED  - UPDATE COMMAND!
+                            //3. TODO: UPDATE THE MYSQL TO REDUCE THE StockQuanaity by the THE AMOUNT REQUESTED  - UPDATE COMMAND!
                                     connection.query("UPDATE products SET ? WHERE ?", [{
                                       quantity: stock
                                     },{
                                       ProductName: product
                                     }], function(err, res) {
-                                      console.log(qty.quantity +" of " + product +"added to cart" );
+                                      console.log("Quantity " + qty.quantity +" of " + product + " added to cart" );
+                                      console.log("");
+                                      console.log(rainbow.r("°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸,ø¤º°`°º¤ø,¸°º¤ø,¸¸,ø¤º°`°º¤ø,¸,ø¤°º¤ø,¸¸"));
+                                      console.log("");
+                  //4. TODO: SHOW THE TABLE again by calling the function that makes the table
+
+                     inquirer.prompt([ {
+                        type: "confirm",
+                        message: "Would you like to continue shopping?",
+                        name: "confirm",
+                        default: true
+
+                      }]).then(function(val) {
+                        //console.log(val);
+                        if (val.confirm == true) {
+                            makeTable();
+                        } else {
+                        console.log("Thank you for shopping at Glamazon! Have a fabulous day!");
+                        process.exit();
+                    }
+            
+                    }); 
                                     });
                                    
                                 }
-                             
+                           
                             });
 
                     });
                     
                 }
+                    });
+                  
+
+                 
 
                
                  
 
-                  //4. TODO: SHOW THE TABLE again by calling the function that makes the table
+                  
+
                       return;
                 }
 
